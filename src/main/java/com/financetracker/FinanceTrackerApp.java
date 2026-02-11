a23943a63a3cea51ec474c27d1b2b237f1f682f8
Semyon Semagin <semyon.semagin.03@mail.ru>
2026-02-12 00:09:49 +0300

    Fonts added

--- src/main/java/com/financetracker/FinanceTrackerApp.java
+++ src/main/java/com/financetracker/FinanceTrackerApp.java
@@ -2,7 +2,13 @@ package com.financetracker;
 
 import com.financetracker.utils.SceneManager;
 import javafx.application.Application;
+import javafx.beans.property.Property;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * Главный класс приложения Finance Tracker
@@ -11,6 +17,10 @@ public class FinanceTrackerApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        System.setProperty("prism.lcdtext", "true");
+        System.setProperty("prism.text", "t2k");
+        loanFonts();
+
         try {
             SceneManager.initialize(primaryStage);
             SceneManager.switchScene("login");
@@ -27,5 +37,12 @@ public class FinanceTrackerApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void loanFonts() {
+        Font.loadFont(Objects.requireNonNull(getClass().getResource("/fonts/Arimo-Bold.ttf")).toExternalForm(), 14);
+        Font.loadFont(Objects.requireNonNull(getClass().getResource("/fonts/Arimo-SemiBold.ttf")).toExternalForm(), 14);
+        Font.loadFont(Objects.requireNonNull(getClass().getResource("/fonts/Arimo-Medium.ttf")).toExternalForm(), 14);
+        Font.loadFont(Objects.requireNonNull(getClass().getResource("/fonts/Arimo-Regular.ttf")).toExternalForm(), 14);
     }
 }
